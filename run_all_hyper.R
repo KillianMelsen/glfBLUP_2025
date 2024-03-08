735134e8f4491c34ba5e0190666670a7c19ba2a5
Melsen <killian.melsen@wur.nl>
2024-03-08 12:10:18 +0100

    update

--- run_all_hyper.R
+++ run_all_hyper.R
@@ -20,3 +20,26 @@ if (!("gfBLUpold" %in% installed.packages())) {
 
 # Step I: data generation ======================================================
 source("hyper/data_generation/generate_hyper_datasets.R"); reset()
+
+# Step II: univariate analysis =================================================
+source("hyper/analyses/univariate_hyper.R"); reset()
+
+# Step III: lsBLUP analysis ====================================================
+source("hyper/analyses/lsBLUP_hyper.R"); reset()
+
+# Step IV: siBLUP analysis =====================================================
+source("hyper/analyses/siBLUP_hyper.R"); reset()
+
+# Step V: gfBLUP analysis ======================================================
+source("hyper/analyses/gfBLUP_hyper.R"); reset()
+
+# Step VI: MegaLMM CV1 analysis ================================================
+source("hyper/analyses/MegaLMM_hyper_CV1_RF.R"); reset()
+
+# Step VII: MegaLMM CV2 analysis ===============================================
+source("hyper/analyses/MegaLMM_hyper_CV2_RF.R"); reset()
+
+# Step VIII: Result Plotting ===================================================
+source("hyper/plot_hyper_results.R"); reset()
+
+
